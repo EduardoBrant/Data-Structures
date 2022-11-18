26e9e5cfc1a9504c283c2462bd1507c2fec845f0
EduardoBrant <carlos23brant@hotmail.com>
2022-11-17 22:40:39 -0400

    update

--- queue/queueArray.c
+++ queue/queueArray.c
@@ -1,1 +1,121 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h> 
+
+struct que
+{
+    int size, front, rear;
+    unsigned actualSize;
+    int* arr; 
+};
+
+struct que* createque(unsigned actualSize)
+{
+    struct que* que = (struct que*)malloc(sizeof(struct que));
+    que -> actualSize = actualSize;
+    que -> front = que -> size = 0;
+    que -> rear = actualSize -1;
+    que -> arr = (int*)malloc(que->actualSize * sizeof(int));
+    return que;
+}
+
+int isFull(struct que *que)
+{
+    return (que->size == que->actualSize);
+}
+
+void enqueue(struct que *que, int item)
+{
+    if(isFull(que)){
+        return;
+    }
+
+    que->rear = (que->rear+1)%que->actualSize;
+    que->arr[que->rear] = item;
+    que->size = que->size+1;
+    printf("\n%d enqueued to que\n",item);
+}
+
+int isEmpty(struct que *que)
+{
+    return (que->size == 0);
+}
+
+int dequeue(struct que *que)
+{
+    if(isEmpty(que)){
+        return INT_MIN;
+    }
+
+    int item = que->arr[que->front];
+    que->front = (que->front+1)%que->actualSize;
+    que->size = que->size-1;
+    return item; 
+}
+
+int front(struct que *que)
+{
+    if(isEmpty(que)){
+        return INT_MIN;
+    }
+    return que->arr[que->front];
+}
+
+int rear(struct que *que)
+{
+    if(isEmpty(que)){
+        return INT_MIN;
+    }
+    return que->arr[que->rear];
+}
+
+int main()
+{
+    int n, val;
+    struct que *que = createque(100);
+
+    do
+    {
+        printf("\n*******************************************************");
+        printf("\n                          MENU                         ");
+        printf("\n*******************************************************");
+        printf("\n1.Enqueue");
+        printf("\n2.Dequeue");
+        printf("\n3.Is empty");
+        printf("\n4.Is full");
+        printf("\n5.First element");
+        printf("\n6.Last element");
+        printf("\n7.Exit");
+        printf("\n\nEnter ur choice : ");
+        scanf("%d",&n);
+        switch(n)
+        {
+            case 1:
+                printf("\nEnter the value: ");
+                scanf("%d",&val);
+                enqueue(que,val);
+                break;
+            case 2:
+                dequeue(que);
+                break;
+            case 3:
+                printf((isEmpty(que)? "\nYes\n" : "\nNot\n"));
+                break;
+            case 4:
+                printf((isFull(que)? "\nYes\n" : "\nNot\n"));
+                break;
+            case 5:
+                printf("\nFront element : %d\n",front(que));
+                break;
+            case 6:
+                printf("\nLast element : %d\n",rear(que));
+                break;
+            case 7: 
+                exit(0);
+                break;
+            default:
+                printf("\nWrong choice !! \n");
+                break;
+        }
+    } while (1);
+}
--- queue/queueLinkedList.c
+++ queue/queueLinkedList.c
@@ -1,1 +1,104 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *next;
+    unsigned data;
+}*p, *tmp, *tmp1;
+
+void insert_end(int);
+void delete_beg();
+void display();
+void isEmpty();
+
+int main()
+{
+    int n, val;
+    p = NULL;
+
+    do
+    {
+        printf("\n*******************************************************");
+        printf("\n                          MENU                         ");
+        printf("\n*******************************************************");
+        printf("\n1.Enqueue");
+        printf("\n2.Dequeue");
+        printf("\n3.Is empty");
+        printf("\n4.Display ");
+        printf("\n5.Exit");
+        printf("\n\nEnter ur choice : ");
+        scanf("%d",&n);
+
+        switch (n)
+        {
+        case 1:
+            printf("\nEnter the value: ");
+            scanf("%d", &val);
+            insert_end(val);
+            break;
+        case 2:
+            delete_beg();
+            break;
+        case 3:
+            isEmpty();
+            break;
+        case 4:
+            display();
+            break;
+        case 5: 
+            exit(0);
+            break;
+        default:
+            printf("\nWrong choice !!\n");
+            break;
+        }
+    } while (1);
+}
+
+void insert_end(int val)
+{
+    tmp = p;
+    tmp1 = (struct node*)malloc(sizeof(struct node));
+    tmp1->data = val;
+    tmp1->next = NULL;
+    
+    if(p == NULL){
+        p = tmp1;
+    }else{
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next= tmp1;
+    }
+}
+
+void isEmpty(){
+    if(p == NULL){
+        printf("\nIs empty\n");
+    }else{
+        printf("\nIs not empty\n");
+    }
+}
+
+void delete_beg()
+{
+    tmp = p;
+    if(p == NULL){
+        printf("\nNo element to be deleted !!\n");
+    }else{
+        printf("\nelement deleted : %d\n", p->data);
+        p = p->next;
+    }
+}
+
+void display()
+{
+    tmp = p;
+    while (tmp != NULL)
+    {
+        printf("\n%d", tmp -> data);
+        tmp = tmp->next;
+    }
+}
